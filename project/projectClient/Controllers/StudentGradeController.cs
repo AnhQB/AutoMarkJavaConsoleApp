5c0b5b76f730b2a9393cbcad2ff11045797f8a4f
Anh Pham <phamanhqb73@gmail.com>
2023-07-20 14:30:25 +0700

    add grade detail

--- project/projectClient/Controllers/StudentGradeController.cs
+++ project/projectClient/Controllers/StudentGradeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OData.Edm;
 using Newtonsoft.Json;
 using project.DTO;
 using project.Models;
+using System.Collections.Generic;
 
 namespace projectClient.Controllers
 {
@@ -21,6 +23,28 @@ namespace projectClient.Controllers
 
                         data = JsonConvert.DeserializeObject<Dictionary<string, ExamResultStudent>>(result);
                         ViewData["examResults"] = data;
+                    }
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DetailAsync(int id, int studentId)
+        {
+            ViewData["examResultId"] = id;
+            ViewData["studentId"] = studentId;
+            List<GradeDetailStudent> data = new List<GradeDetailStudent>();
+            string link = "http://localhost:5000/api/Grade/GradeDetail";
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(link + "?examresultId=" + id))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string result = await content.ReadAsStringAsync();
+
+                        data = JsonConvert.DeserializeObject<List<GradeDetailStudent>>(result);
+                        ViewData["details"] = data;
                     }
                 }
             }
